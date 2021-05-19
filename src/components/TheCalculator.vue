fae0aa44335ae81318acd8f4038b12cdb0ac42df
Jesuseun Jeremiah Olatunde <jerryolatunde@gmail.com>
2021-05-19 15:52:00 +0100

    Added and configured Numeric keypad component

--- src/components/TheCalculator.vue
+++ src/components/TheCalculator.vue
@@ -1,11 +1,13 @@
 <template>
 	<div id="calculator">
 		<TheDisplay v-bind:data="currData"></TheDisplay>
+		<TheNumericPad @btn-press="updateData"></TheNumericPad>
 	</div>
 </template>
 
 <script>
 	import TheDisplay from "./TheDisplay.vue";
+	import TheNumericPad from "./TheNumericPad.vue";
 	export default {
 		data(){ return {
 			currData : "0",
@@ -13,7 +15,18 @@
 		}},
 		components : {
 			TheDisplay,
+			TheNumericPad,
 		},
+		methods : {
+			updateData(digit){
+				if( this.currData.endsWith(".") && digit == "." ) return;
+				else if( this.currData == "0" && digit == "0" ) return;
+				else if( this.currData.includes(".") && digit == "." ) return;
+				else if( this.currData.length == 7 ) return;
+				else if( this.currData == "0" ) this.currData = digit;
+				else this.currData += digit;
+			}
+		}
 	}
 </script>
 
@@ -23,15 +36,15 @@
 		height: 570px;
 		background-color: rgb( 70, 70, 70, );
 		display: grid;
-		grid-template-columns: global.repeat( auto, 4 );
+		grid-template-columns: global.repeat( 90px, 4 );
 		grid-template-rows: 120px global.repeat( auto, 5 );
 		grid-template-areas:
 			"display display display display"
 			". . . ."
-			". . . ."
-			". . . ."
-			". . . ."
-			". . . ."
+			"numeric numeric numeric ."
+			"numeric numeric numeric ."
+			"numeric numeric numeric ."
+			"numeric numeric numeric ."
 		;
 	}
 </style>
---
+++ src/components/TheNumericPad.vue
@@ -0,0 +1,46 @@
+<template>
+	<div id="numeric" @click="test">
+		<div 
+			v-for="(item, idx) of content" 
+			:key="idx" 
+			class="item"
+			:data-digit = "item"
+		>
+			<div>{{item}}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){ return{
+			content : [ ".", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ].reverse()
+		}},
+		methods : {
+			test(e){
+				if(e.target.dataset.digit) this.$emit( "btn-press", e.target.dataset.digit)
+				else this.$emit( "btn-press", e.target.parentElement.dataset.digit)
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	#numeric{
+		grid-area: numeric;
+		height: 360px;
+		display: flex;
+		flex-wrap: wrap;
+		align-content: center;
+		background-color: rgb( 20, 20, 20 );
+		font-family: "Orbitron-Bold";
+		font-size: 1.5em;
+	}
+	.item[data-digit="0"]{ flex-basis: 180px;}
+	.item{ 
+		@include global.position(center); 
+		flex-basis: 90px;
+		height: 90px;
+		border: 1px solid rgb( 0, 0, 0 );
+	}
+</style>
